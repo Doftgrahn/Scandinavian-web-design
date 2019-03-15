5772ca1540cda9c6510e2516731dc83cedc5c71b
Simon grahn <simon.grahn@gmail.com>
2019-03-15 16:34:27 +0100

    started on header

---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+export class Header extends Component {
+
+render() {
+  const header = ['Home', 'About', 'Our Philosophy','Contact'];
+  const headerLinks = header.map(page=> {
+    return (
+      <a className="headerLinks" key= {page} href= {'/'+page}>{page}</a>)
+  })
+  return <div className="Header"><nav>{headerLinks}</nav> </div>;
+  }
+}
